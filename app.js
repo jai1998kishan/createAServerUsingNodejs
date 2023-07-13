71f7011a3108e680f4b8d31c8a22f6e3ab623b27
jai1998kishan <jaikishangupta754@gmail.com>
2023-07-13 15:09:04 +0530

    addingResponses

--- app.js
+++ app.js
@@ -14,14 +14,24 @@ const http=require('http');
 
 
 //you can also use arrow function
-// const server=http.createServer((req,res)=>{
-//     console.log(req);
-// });
-
-// server.listen(3000);
-
 const server=http.createServer((req,res)=>{
-    console.log("Jaikishan Gupta");
-})
+    console.log(req.url,req.method,req.headers);
+    // process.exit();           //it will exit the server and know one able to access it again
+    res.setHeader('content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js server!</h1><body>');
+    res.write('</html>');
+    res.end();
+    // res.write()   //it will show error  if we add the `end` than we are not able to write anything after that
 
-server.listen(4000);
+
+});
+
+server.listen(3000); 
+
+// const server=http.createServer((req,res)=>{
+//     console.log("Jaikishan Gupta");
+// })
+
+// server.listen(4000);
