7ab5760256be8df5715926d7d09d3eb0754c1204
jai1998kishan <jaikishangupta754@gmail.com>
2023-07-13 21:02:55 +0530

    problem1

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http=require('http');
+const fs=require('fs');
 
 //you can also create that
 // function rqListener(req,res){
@@ -15,8 +16,42 @@ const http=require('http');
 
 //you can also use arrow function
 const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+    // console.log(req);
+    // console.log(req.url);
+    // console.log(req.method);
+    // console.log(req.headers);
+    // console.log(req.url,req.method,req.headers);
+    
     // process.exit();           //it will exit the server and know one able to access it again
+    
+    const url=req.url;
+    const method=req.method;
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Msg</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form><body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==='/message' && method==='POST'){
+        const body=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        })
+        return req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString();
+            const message=parsedBody.split('=')[1];
+            fs.writeFile('message.text',message,(err)=>{
+                res.statusCode=302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
+            
+        })
+        
+        
+    }
     res.setHeader('content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title><head>');
---
+++ problem1.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === '/home') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome home');
+  } else if (url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to About Us page');
+  } else if (url === '/node') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to my Node.js project');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+server.listen(4000, () => {
+  console.log('Server running on port 4000');
+});
